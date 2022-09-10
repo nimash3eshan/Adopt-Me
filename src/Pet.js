4d62cbc3299739e2f18e40f15bf5684c16f8081d
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-09-10 12:08:41 +0530

    searchParams

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,25 @@
-import React from "react";
+// import React from "react";
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
+// export default function Pet({ name, animal, breed }) {
+//.   return React.createElement("div", {}, [
+//.     React.createElement("h1", {}, name),
+//.     React.createElement("h2", {}, animal),
+//.     React.createElement("h2", {}, breed),
+//   ]);
+// }
+
+//z  Lets use JSX 
+
+const Pet = (props) => {
+
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed}</h1>
+    </div>
+  );
+
 }
+
+export default Pet;
--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
-import React from "react";
+// import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams"
 
+
+// const App = () => {
+//.   return React.createElement("div", {}, [
+//.     React.createElement("h1", { id: "brand" }, "Adopt Me!"),
+//.     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//.       breed: "Havanese",
+//     }),
+//.     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//.       breed: "Cockatiel",
+//     }),
+//.     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
+//   ]);
+// };
+
+
+//. render(React.createElement(App), document.getElementById("root"));
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" /> */}
+
+      <SearchParams/>
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
